3b04d1b3b065f4064c84a9f0a4d573de96f0800e
Evan Burnette <EvanBurnette@users.noreply.github.com>
2021-12-26 22:16:16 -0800

    Created a more general purpose width reset with !important;

--- widthResetForAOCWebsite.js
+++ widthResetForAOCWebsite.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Remove Advent of Code hardcoded width
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  remove hardcoded width on advent of code
-// @author       You
+// @author       Evan Burnette
 // @match        https://adventofcode.com/2021/day/*
 // @icon         https://www.google.com/s2/favicons?domain=adventofcode.com
 // @grant        none
@@ -12,7 +12,7 @@
 (function() {
   'use strict';
   const style = document.createElement("style");
-  const payload = document.createTextNode("article{max-width: 95vw;}");
+  const payload = document.createTextNode("* {max-width: 95vw !important;}");
   style.appendChild(payload);
   document.body.appendChild(style);
 })();
